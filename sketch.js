4743e11e80f69e09b352c50c2e25ec2161145f54
Manjula Singh <MS@vinods-MacBook-Pro.local>
2020-08-28 15:38:51 +0530

    final

--- sketch.js
+++ sketch.js
@@ -6,14 +6,15 @@ var dog,happydDog,database,foodStock,foodS;
 
 function preload(){
   //load images here
-  dogimg = loadImage("images/dogImg.png");
-  dogimg2 = loadImage("images/dogImg1.png");
+  dogimg = loadImage("dogImg.png");
+  dogimg2 = loadImage("dogImg1.png");
 }
 
 function setup() {
-  var canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  var canvas = createCanvas(500, 500);
   database = firebase.database();
-  foodStock = database.ref('food');
+  foodStock = database.ref('Food');
+  foodStock.on("value", readStock);
 
   //foodStock.on("value",readStock);
   dog = createSprite(250,250,10,10);
@@ -26,32 +27,45 @@ function setup() {
 function draw() {  
 background(46, 139, 87);
 if(keyWentDown(UP_ARROW)){
-  //writeStock(foodS);
+  writeStock(foodS);
   dog.addImage(dogimg2);
 }
-
-
-
-
-
   drawSprites();
   //add styles here
-  fill("red");
-  textSize(20);
-  stroke(5);
-  text("Press Up Arrow Key To Feed drago milk",100,100);
-  
-  
-  
+  fill(255);
+  stroke(0);
+  strokeWeight(5);
+  textSize(15);
+  text("Food remaning: " + foodS, width/2-50, height/2-100);
 
 }
-function readStock(data){
-  foodS=data.val();
-}
-function writeStock(x){
-database.ref('/').update({
-  food:x
-})
+
+function readStock(data) 
+{
+
+  foodS = data.val();
+
 }
 
+function writeStock(x)
+{
 
+  if (x <= 0) 
+  {
+
+    x = 0;
+
+  } else 
+  {
+
+    x = x - 1;
+
+  }
+
+  database.ref('/').update({
+
+    Food:x
+    
+  })
+
+}
